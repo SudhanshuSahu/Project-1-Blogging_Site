7bf1cfaff70ccc979990dbb10beeef184c77e096
MohitRaykwar7389966852 <mohitraykwar99@gmail.com>
2022-04-27 16:08:08 +0530

    update blog

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -3,17 +3,19 @@ const authorModel = require("../Model/authorModel");
 
 const createBlog = async function (req, res) {
     let data = req.body
+    if(data.hasOwnProperty('ispublished'))
+    {
+        if (data.ispublished == true) {
+            data.publishedAt = Date.now()
+        }
+    }
     let authorId = data.authorId
     let checkAuthorId = await authorModel.find({ _id: authorId })
     if (!checkAuthorId) {
-        return res.status(400).send({ status: false, msg: "Enter valid Author Id" })
+        res.status(400).send({ status: false, msg: "Enter valid Author Id" })
     }
     let createBlogData = await blogModel.create(data)
-    if (createBlogData.ispublished == true) {
-        let blogUpdated = await blogModel.findOneAndUpdate({ _id: createBlogData._id }, { publishedAt: Date.now() }, { new: true })
-        return res.status(201).send({ status: true, data: blogUpdated })
-    }
-    return res.status(201).send({ status: true, data: createBlogData })
+    res.status(201).send({ status: true, data: createBlogData })
 }
 
 module.exports.createBlog = createBlog
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const authorController = require("../controller/authorController")
 const blogController= require("../controller/blogController")
 const getBlogController= require("../controller/getBlogController")
+const putBlogController= require("../controller/putBlogController")
 
 router.post("/createauthor", authorController.createAuthor)
 router.post("/createblog",blogController.createBlog )
 router.get("/getblog",getBlogController.getBlog)
+router.put("/blogs/:blogid",putBlogController.putBlog)
 
 
 module.exports = router;
